7c740180b3d28834bc9747d8114cf9e704e65ff5
Gil <gilbertjvirgo@gmail.com>
2023-11-10 09:59:32 +0000

    Added facebook seo

--- pages/carols.js
+++ pages/carols.js
@@ -1,11 +1,33 @@
 import CarolServiceBanner from "@/components/CarolServiceBanner";
 import CarolServiceInfo from "@/components/CarolServiceInfo";
 import Head from "next/head";
+import NextSeo from "next-seo";
 import Section from "@/components/Section";
 
 export default () => {
 	return (
 		<>
+			<NextSeo
+				title="Carols | Victoria Park Community Church"
+				description="We invite you to come join others from the local community to sing Christmas carols, eat mince pies, and hear what it's all about!"
+				canonical="https://vpcc.church/carols"
+				openGraph={{
+					url: "https://vpcc.church/carols",
+					title: "Carols | Victoria Park Community Church",
+					description:
+						"We invite you to come join others from the local community to sing Christmas carols, eat mince pies, and hear what it's all about!",
+					images: [
+						{
+							url: "https://vpcc.church/assets/sm-carols-image.jpg",
+							width: 2000,
+							height: 1200,
+							alt: "Carols | Victoria Park Community Church",
+							type: "image/jpeg",
+						},
+					],
+					siteName: "Victoria Park Community Church",
+				}}
+			/>
 			<Head>
 				<title>Carols | Victoria Park Community Church</title>
 			</Head>
